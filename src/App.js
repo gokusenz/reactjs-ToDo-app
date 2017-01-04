aa23c23a41662bc600e22585ada4aeeb85f309d1
Nattawut <nattawut@thinknet.co.th>
2017-01-04 16:11:20 +0700

    destruceting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser="Nattawut" />
         <div>
           Hello
         </div>
--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,9 @@ import React, {Component} from "react"
 
 class Header extends Component {
     render(){
+        let { currentUser, isLoggedIn } = this.props
         return (
-            <div>Header</div>
+            <div>Header {this.props.currentUser}</div>
         )
     }
 }
