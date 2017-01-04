dff7cab3fc1d39c54e0c9f9ef6ac52da38904dfb
Nattawut <nattawut@thinknet.co.th>
2017-01-04 16:15:04 +0700

    extend string

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,8 @@ import React, {Component} from "react"
 class Header extends Component {
     render(){
         let { currentUser, isLoggedIn } = this.props
+
+        currentUser = "Logged in as " + currentUser
         return (
             <div>Header {currentUser} {isLoggedIn}</div>
         )
