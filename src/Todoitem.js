ff6bef27f8e23de312f8ff8fc26ff5a9218a9c62
Nattawut <nattawut@thinknet.co.th>
2017-01-04 17:24:22 +0700

    add toggle

--- src/Todoitem.js
+++ src/Todoitem.js
@@ -1,11 +1,31 @@
 import React, {Component} from "react"
 
 class Todoitem extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            done: false
+        }
+        this.toggle = this.toggle.bind(this)
+    }
+
+    toggle() {
+        this.setState({
+            done: !this.state.done
+        })
+    }
+    
     render() {
         let { text } = this.props
+        
         return (
             <div>
-            {text}
+            <input type="checkbox" name={text} 
+                value={this.state.done} 
+                onClick={this.toggle}
+            />
+            {this.state.done ? "Done: " + text : text}
             </div>
         )
     }
