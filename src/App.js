05700ae94605140c763b045d2fa132c492ec0f44
Nattawut <nattawut@thinknet.co.th>
2017-01-04 16:04:25 +0700

    add Header class

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <Header/>
+        <div>
+          Hello
         </div>
-        <p className="App-intro">
-          Hello World.
-        </p>
       </div>
-    );
+    )
   }
 }
 
