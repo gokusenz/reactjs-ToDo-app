173c1b652ac037603d06d1e971e74554defe126c
Nattawut <nattawut@thinknet.co.th>
2017-01-04 16:23:59 +0700

    counter time with state

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,25 @@
 import React, {Component} from "react"
 
 class Footer extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            time: 0
+        }
+        setInterval(() => {
+            this.setState({
+                time: this.state.time+1
+            })
+        }, 1000)
+    }
+
     render(){
+        let {time} = this.state
         return (
-            <div>Footer</div>
+            <div>Footer: Online Time {time}</div>
         )
     }
 }
 
-export default Footer
+export default Footer;
